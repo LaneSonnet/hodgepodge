3ed8d9ec22f4b0f7a20e652fe668b16997a84ff6
jiangweigen <1548346934@qq.com>
2019-01-13 13:24:00 +0800

    springboot eureka

--- springCloud/ms-movie/src/main/java/com/jiangwg/MsMovieApplication.java
+++ springCloud/ms-movie/src/main/java/com/jiangwg/MsMovieApplication.java
@@ -2,6 +2,7 @@ package com.jiangwg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -9,6 +10,7 @@ import org.springframework.web.client.RestTemplate;
  * Created by Mudfish on 2019/1/5 0005.
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class MsMovieApplication {
 
 	@Bean
--- springCloud/ms-user/src/main/java/com/jiangwg/controller/UserController.java
+++ springCloud/ms-user/src/main/java/com/jiangwg/controller/UserController.java
@@ -1,12 +1,16 @@
 package com.jiangwg.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jiangwg.entity.User;
 import com.jiangwg.repository.UserRepository;
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
 
 @RestController
 public class UserController {
@@ -14,8 +18,27 @@ public class UserController {
   @Autowired
   private UserRepository userRepository;
 
+  @Autowired
+  private EurekaClient eurekaClient;
+
+  @Autowired
+  private DiscoveryClient discoveryClient;
+
   @GetMapping("/simple/{id}")
   public User findById(@PathVariable Long id) {
     return this.userRepository.findOne(id);
   }
+
+  @GetMapping("/eureka-instance")
+  public String serviceUrl() {
+    InstanceInfo instance = this.eurekaClient.getNextServerFromEureka("MS-USER", false);
+    return instance.getHomePageUrl();
+  }
+
+  @GetMapping("/instance-info")
+  public ServiceInstance showInfo() {
+    ServiceInstance localServiceInstance = this.discoveryClient.getLocalServiceInstance();
+    return localServiceInstance;
+  }
+
 }
